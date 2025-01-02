66a6a4af7a2f6584a7be8f28a815bd25d8ca10a0
Tom Wheatley <tomgw1@gmail.com>
2025-01-02 23:07:25 +0000

    gtag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { AuroraBackground } from "@/components/ui/aurora-background";
+import { sendGTMEvent } from "@next/third-parties/google";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,11 +21,15 @@ export default function Home() {
           Thirsty Souls
         </div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          coming soon
+          Coming soon
         </div>
-        {/* <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button> */}
+        <Link href="https://sedo.com/search/details/?partnerid=14460&language=e&domain=thirstysouls.co.uk&origin=parking&utm_medium=Parking&utm_campaign=template&utm_source=3199">
+          <button
+          onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'buy domain' })}
+          className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+            Buy this domain
+          </button>
+        </Link>
       </motion.div>
     </AuroraBackground>
   );
